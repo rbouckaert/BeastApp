3142889a9b84c905bc6c74ee6e844fe8f8c25671
Remco Bouckaert <higgs.ml@gmail.com>
2016-06-06 19:46:42 +1200

    move web page to github.io

--- src/examples/MiniTracerClassic.java
+++ src/examples/MiniTracerClassic.java
@@ -41,50 +41,33 @@ public class MiniTracerClassic extends Runnable {
 		Double [] data = traces.getTrace(trace);
 		Double [] labels = traces.getTrace("Sample");
 		
+		// create plot.ly chart see http://plot.ly/ for documentation
 		String html = "<html>\n" + 
 		"<title>BEAST " + new BEASTVersion2().getVersionString() + ": miniTracer</title>\n" +
-		"<head>  \n" +
-		"  <script type=\"text/javascript\">\n" +
-		"  window.onload = function () {\n" +
-		"\n" +
-		"    var chart = new CanvasJS.Chart(\"chartContainer\",\n" +
-		"    {\n" +
-		"      zoomEnabled: true, \n" +
-		"      title:{\n" +
-		"        text: \"Trace of " + trace + " from " + tracelog.getPath() +"\"\n" +
-		"      },\n" +
-		"      axisY:{\n" +
-		"        includeZero: false\n" +
-		"      },\n" +
-		"      data: data  // data from below\n" +
-		"   });\n" +
-		"\n" +
-		"    chart.render();\n" +
-		"  }\n" +
-		"       \n" +
-		"    \n" +
-		"    var x= " + Arrays.toString(labels)+ " \n" +
-		"    var y= " + Arrays.toString(data)+ " \n" +
-		"    var data = []; var dataSeries = { type: \"line\" };\n" +
-		"    var dataPoints = [];\n" +
-		"    for (var i = 0; i < x.length; i += 1) {\n" +
-		"         dataPoints.push({\n" +
-		"          x: x[i],\n" +
-		"          y: y[i]\n" +
-		"           });\n" +
-		"        }\n" +
-		"     dataSeries.dataPoints = dataPoints;\n" +
-		"     data.push(dataSeries);               \n" +
-		"  \n" +
-		"  </script>\n" +
-		"  <script type=\"text/javascript\" src=\"canvasjs.min.js\"></script></head>\n" +
+		"<head>\n" +
+		"<script src='plotly-latest.min.js'></script>\n" +
+		"</head>\n" +
 		"<body>\n" +
-		"    <div id=\"chartContainer\" style=\"height: 90%; width: 100%;\">\n" +
-		"    </div>\n" +
-		"<p>Try dragging your mouse across the chart to zoom inside charts.</p>\n" + 
+		"<h2>Trace of " + trace + " from " + tracelog.getPath() +"</h2>\n" +
+		"<div id='chart'/>\n" +
+		"<script>\n" +
+		"var data = [\n" +
+		"  {\n" +
+		"    x: " + Arrays.toString(labels)+ ",\n" +
+		"    y: " + Arrays.toString(data)+ ",\n" +
+		"    type: 'scatter'\n" +
+		"  }\n" +
+		"];\n" +
+		"\n" +
+		"Plotly.newPlot('chart', data);\n" +
+		"</script>\n" +
+		"<p>This is a simple graph with all settings at default. "
+		+ "Click the 'edit' button (which appears at the top of the graph when you hover over the graph) "
+		+ "to change the colours and other styles. "
+		+ "To see how the javascripts changes, click the JSON button in the style editor.</p>\n" +
 		"</body>\n" +
-		"</html>";		
-		
+		"</html>";
+				
 		// write html file in package dir + "/js/minitracer.html"
 		String jsPath = Application.getPackagePath("BeastApp.addon.jar") + "js";
         FileWriter outfile = new FileWriter(jsPath + "/minitracer.html");
